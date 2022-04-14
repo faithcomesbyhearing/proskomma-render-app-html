4636e93a359a087947178c810b4965530b0f2d75
Mark Howe <mvahowe@gmail.com>
2022-04-14 11:42:56 +0200

    Subclass DocSet and Document models

--- scripts/make_chapters.js
+++ scripts/make_chapters.js
@@ -1,7 +1,9 @@
 import fse from 'fs-extra';
 import path from 'path';
 
-import { ScriptureParaModel, ScriptureDocSet, ScriptureParaDocument } from 'proskomma-render';
+import { ScriptureParaModel } from 'proskomma-render';
+import AppHtmlDocSetModel from "../AppHtmlDocSetModel.js";
+import AppHtmlDocumentModel from "../AppHtmlDocumentModel.js";
 
 if (process.argv.length !== 3) {
     throw new Error("USAGE: node make_chapter.js <srcDirPath>");
@@ -16,8 +18,8 @@ const toRender = fse.readJsonSync(toRenderPath);
 
 const context = {};
 const config = {};
-const documentModel = new ScriptureParaDocument(toRender, context, config);
-const docSetModel = new ScriptureDocSet(toRender, context, config);
+const documentModel = new AppHtmlDocumentModel(toRender, context, config);
+const docSetModel = new AppHtmlDocSetModel(toRender, context, config);
 const processingModel = new ScriptureParaModel(toRender, config);
 docSetModel.addDocumentModel('default', documentModel);
 processingModel.addDocSetModel('default', docSetModel);
