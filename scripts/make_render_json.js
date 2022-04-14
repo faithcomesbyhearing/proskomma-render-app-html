a35e4a9c099b2152d178cb1932c192335a6d7368
Mark Howe <mvahowe@gmail.com>
2022-04-14 10:59:32 +0200

    Script to produce JSON to render from Succinct

---
+++ scripts/make_render_json.js
@@ -0,0 +1,19 @@
+import fse from 'fs-extra';
+import path from 'path';
+
+import {UWProskomma} from 'uw-proskomma';
+import {ScriptureParaModelQuery} from 'proskomma-render';
+
+if (process.argv.length !== 3) {
+    throw new Error("USAGE: node make_render_json.js <srcDirPath>");
+}
+
+const srcDirPath = process.argv[2];
+
+const succinct = fse.readJsonSync(path.resolve(path.join(srcDirPath, "succinct.json")));
+const pk = new UWProskomma();
+pk.loadSuccinctDocSet(succinct);
+
+const result = await ScriptureParaModelQuery(pk, [], []);
+
+fse.writeJsonSync(path.resolve(path.join(srcDirPath, "toRender.json")), result);
